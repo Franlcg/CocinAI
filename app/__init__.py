4038c0866d5f290bd85e895308f28817782fe725
Franlcg <franciscoluiscg@outlook.com>
2025-04-03 21:08:46 +0200

    Intentando poner el modelo entrenado en el proyecto

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@
 from flask import Flask
 from dotenv import load_dotenv
 import os
+from .imagen import imagen_blueprint  # Importar blueprint de imagen
+from .modelo_gpt2 import gpt2_blueprint  # Importar blueprint de imagen
 
 # Cargar variables de entorno desde el archivo .env (si lo usas)
 load_dotenv()
@@ -12,7 +14,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Crear la instancia de la aplicación Flask
 app = Flask(__name__)
-
+app.register_blueprint(imagen_blueprint, url_prefix="/select-ingredients")
+app.register_blueprint(gpt2_blueprint, url_prefix="/gpt2")
 # Importar los routers para que se registren las rutas
 from app import routes
-from app import imagen
